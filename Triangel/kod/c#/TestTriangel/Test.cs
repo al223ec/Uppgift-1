65ab630e0a1097b0ad5f2c0ad81483d8b58ada2e
al223ec <al223ec@student.lnu.se>
2013-11-12 12:55:19 +0100

    Testprojekt skapat

---
+++ Triangel/kod/c#/TestTriangel/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestTriangel
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void isIsoscelesTest()
+        {
+            Triangle t = new Triangle(1.0,0.5,1.0); 
+            Assert.IsTrue(t.isIsosceles());
+        }
+    }
+}
