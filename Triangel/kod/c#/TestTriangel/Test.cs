32dd8dd21e0c8998c8b995a315645f7d9f4c61e1
al223ec <al223ec@student.lnu.se>
2013-11-12 13:03:25 +0100

    Testprojekt skapat mer filer

--- Triangel/kod/c#/TestTriangel/Test.cs
+++ Triangel/kod/c#/TestTriangel/Test.cs
@@ -12,5 +12,16 @@ namespace TestTriangel
             Triangle t = new Triangle(1.0,0.5,1.0); 
             Assert.IsTrue(t.isIsosceles());
         }
+
+        public void isEquilateralTest()
+        {
+
+        }
+
+        public void isScaleneTest()
+        {
+
+        }
+
     }
 }
